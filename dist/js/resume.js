3eb466ae52e4b61f6820119626c3f6180cffc46b
Masper <casper.rang@gmail.com>
2019-06-09 15:32:06 +0200

    changed resume again

--- dist/js/resume.js
+++ dist/js/resume.js
@@ -1,5 +1,9 @@
 // Select DOM items
-const resumeSkills = document.querySelectorAll(".skills > li");
+const resumeSkills = document.querySelectorAll(".score");
+
+let done = false;
+let animationAmount = 0;
+let speed = 200; // lower is faster
 
 // Boolean of no-styling standard set to false
 let showSkillsStyling = false;
@@ -9,17 +13,22 @@ function toggleShowSkills() {
 }
 
 function showStyling() {
-  if (!showSkillsStyling) {
-    for (skill of resumeSkills) {
-      let number = skill.className;
-      console.log(number);
-      skill.innerHTML += ": ";
+  if (animationAmount == 100) {
+    done = true;
+  }
 
-      for (i = 0; i < number; i++) {
-        skill.innerHTML += "OOO";
-      }
+  if (!done) {
+    animationAmount += 1;
+    for (skill of resumeSkills) {
+      let number = skill.innerHTML;
+      let canvas = skill.getContext("2d");
+      console.log(number);
+      canvas.fillRect(0, 2, number * 50 * (animationAmount / 100), 40);
     }
+
+    return setInterval(showStyling, speed);
   } else {
+    return;
   }
 }
 
